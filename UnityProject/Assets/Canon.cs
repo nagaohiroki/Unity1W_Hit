2be6b4a9bc98789e04b1f637a2fb8ad6822f8302
長尾 洋樹 <nagao_hiroki@hand.co.jp>
2018-02-21 14:05:12 +0900

    add UnityProject

---
+++ UnityProject/Assets/Canon.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class Canon : MonoBehaviour
+{
+	[SerializeField]
+	Text mDebug;
+
+	[SerializeField]
+	GameObject mBullet;
+
+	void Start()
+	{
+		mBullet.SetActive(false);
+	}
+	void Update()
+	{
+		RotMouseCursor();
+		if(Input.GetButtonDown("Fire1"))
+		{
+			GenerateBullet();
+		}
+	}
+	void RotMouseCursor()
+	{
+		var pos = MousePosition();
+		var vec = pos - transform.position;
+		mDebug.text = pos.ToString();
+		var euler = transform.eulerAngles;
+		euler.z = Mathf.Clamp(Mathf.Rad2Deg * Mathf.Atan2(vec.y, vec.x), 0.0f, 90.0f);
+		transform.eulerAngles = euler;
+	}
+	Vector3 MousePosition()
+	{
+		var cam = Camera.main;
+		var vec =Input.mousePosition;
+		vec.z = -cam.transform.position.z;
+		return cam.ScreenToWorldPoint(vec);
+	}
+	void GenerateBullet()
+	{
+		if(mBullet == null)
+		{
+			return;
+		}
+		var bullet = Instantiate(mBullet);
+		bullet.SetActive(true);
+		bullet.transform.position = transform.position;
+		var rigid = bullet.GetComponent<Rigidbody>();
+		rigid.AddForce(transform.forward);
+	}
+}
---
+++ UnityProject/Assets/Test.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+public class Test : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		
+		var vec =Input.mousePosition;
+		vec.z = -Camera.main.transform.position.z;
+		var pos = Camera.main.ScreenToWorldPoint(vec);
+		transform.position = pos;
+	}
+}
